b77dae912167f24155f1922c9bb64afc96ac3252
Daniel Bontii <daniel.bontii@amalitech.org>
2021-08-11 17:23:46 +0000

    scripts added later

---
+++ script.js
@@ -0,0 +1,18 @@
+//Initialize my age to 24
+const myAge = 24;
+
+//Initialize early years to 2
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+//Subtract 2 from my age
+let laterYears = myAge - 2;
+//multiply late years by 4
+laterYears = laterYears * 4;
+
+//calculate my age in dog years by adding early and late years
+myAgeInDogYears = earlyYears + laterYears;
+
+myName = 'Daniel Bontii'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
